95433a429cfd67ca13712094bb7e182da4ac2a6e
Sandy Lee <sl30724@gmail.com>
2022-09-08 16:50:51 -0600

    updated assignment-1

--- assignment-1/main.js
+++ assignment-1/main.js
@@ -1,10 +1,10 @@
 const buttons = document.querySelectorAll(".btn");
 for (const button of buttons) {
-    button.addEventListener("mouseover", () =>{
-        button.style.color = "red";
-        button.style.backgroundColor = "black";
+    button.addEventListener("mouseover", () => {
+        button.style.color = "black";
+        button.style.backgroundColor = "yellow";
     })
-    button.addEventListener("mouseout", () =>{
+    button.addEventListener("mouseout", () => {
         button.style.color = "black";
         button.style.backgroundColor = "lightgray";
     })
@@ -16,23 +16,31 @@ let y;
 
 const heading = document.getElementById("heading");
 
-if (x == 69){
-    console.log("success");
-    heading.innerText = "That's the right number!";
-    heading.style.backgroundColor = "green";
-    heading.style.color = "white";
+function success() {
+    if (x == 69) {
+        console.log("success");
+        heading.innerText = "That's the right number!";
+        heading.style.backgroundColor = "green";
+        heading.style.color = "white";
+    } else {
+        heading.innerText = "Keep guessing...";
+        heading.style.backgroundColor = "white";
+        heading.style.color = "black";
+    }
 }
 
 const up = document.getElementById("up");
-up.addEventListener("click", () =>{
-    y=x+1;
+up.addEventListener("click", () => {
+    y = x + 1;
     number.innerText = y;
-    x=y;
+    x = y;
+    success();
 })
 
 const down = document.getElementById("down");
-down.addEventListener("click", () =>{
-    y=x-1;
+down.addEventListener("click", () => {
+    y = x - 1;
     number.innerText = y;
-    x=y;
+    x = y;
+    success();
 })
