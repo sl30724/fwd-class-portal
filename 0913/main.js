63872bb6c5580459debb02bbd94d7055f8f5f6de
Sandy Lee <sl30724@gmail.com>
2022-09-15 10:32:36 -0600

    0915 in-class updates

--- 0913/main.js
+++ 0913/main.js
@@ -17,3 +17,9 @@ firstTextInput,addEventListener("change", () => {
 //see type of objects or elements this way!
 console.log(typeof firstTextInput);
 console.log(typeof 2);
+
+// disable form from refreshing the page when you press enter
+const myForm = document.getElementById("my-form");
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
