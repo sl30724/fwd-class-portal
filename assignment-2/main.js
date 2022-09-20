ce2ea6e00b82dedf7032adc70d7a36ec0fffe278
Sandy Lee <sl30724@gmail.com>
2022-09-20 10:56:08 -0600

    Update main.js

--- assignment-2/main.js
+++ assignment-2/main.js
@@ -2,7 +2,6 @@ const submitBtn = document.getElementById("submit-button");
 const result = document.getElementById("result");
 const result1 = document.getElementById("result-1");
 const form = document.getElementById("bubbletea");
-let toppingList = [];
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -14,45 +13,54 @@ form.addEventListener("reset", () => {
 
 form.addEventListener("submit", () => {
     let orderName = document.getElementById("order-name");
-    // console.log(orderName.value)
+    console.log(orderName.value)
     let tea = document.querySelector('input[name="tea"]:checked');
-    // console.log(tea.value);
+    console.log(tea.value);
     let milk = document.querySelector('input[name="milk"]:checked');
-    // console.log(milk.value);
-    let toppings = document.getElementsByName("toppings");
+    console.log(milk.value);
+
 
     if (orderName != null && tea != null && milk != null) {
         result.innerText = `Hi ${orderName.value}, you ordered a ${milk.value} ${tea.value} with `;
-        
-        for (let topping of toppings) {
-            if (topping.checked) {
-                result.append(topping.value + " ");
-                console.log(topping.value);
-                toppingList.push(topping);
-                console.log(toppingList);
-            }
-        }
-         if (toppingList = []) {
+
+        let toppings = document.querySelectorAll('input[name="toppings"]:checked');
+        let toppingValues = [];
+        toppings.forEach((topping) => {
+            toppingValues.push(topping.value);
+            result.append(topping.value + " ");
+
+        // Another way to do this! which is basically the same
+        // let toppings = document.getElementsByName("toppings");
+        // let toppingList = [];
+        // for (let topping of toppings) {
+        //     if (topping.checked) {
+        //         result.append(topping.value + " ");
+        //         toppingList.push(topping);
+        //     }
+        // }
+        });
+
+        if (toppingValues.length === 0) {
             result.append("nothing");
-         }
+        }
+
+        // if (toppings == false) {
+        //     result.append("nothing");
+        //     // this will not return "nothing"
+        // }
+
+        // if (toppings == null){
+        //     result.append("nothing");
+        //     // this also will not return "nothing"
+        // }
+
+        // if (toppings.value == null){
+        //     result.append("nothing");
+        //     // this will always return "nothing" 
+        // }
+
+
     } else {
         result.innerText = "Oops! You haven't finished your order. Make sure you've answered all questions.";
     }
 })
-
-// submitBtn.addEventListener("clicked", () => {
-//     let orderName = document.getElementById("order-name").value;
-//     console.log(orderName);
-//     let tea = document.querySelector('input[name="tea"]:checked');
-//     console.log(tea);
-//     let milk = document.querySelector('input[name="milk"]:checked');
-//     console.log(milk);
-//     let toppings = document.querySelector('input[name="toppings"]:checked');
-//     console.log(toppings);
-//     result.innerText = "You completed your order!";
-    // if (orderName === null || tea == false || milk == false || toppings == false){
-    //     result.innerText = "You haven't finished your order! Make sure you've answered all questions.";
-    // } else {
-    //     result.innerText = "You finished your order!"
-    // }
-// })
